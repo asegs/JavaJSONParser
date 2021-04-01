2b201c19815e2cd9ff5aaf61c89b5425cb30c5a5
Aaron <54863852+asegs@users.noreply.github.com>
2021-04-01 10:52:37 -0400

    Gave expected result for responses

--- Examples.java
+++ Examples.java
@@ -3,9 +3,14 @@ public class Driver {
         Parser p = new Parser();
         String carsJSON = "{'name':'John','age':30,'cars': {'car1':'Ford','carData':'BMW','car3':'Fiat','interior':{'material':'leather','color':'blue','keys':{'digital':'yes','physical':'no'}}},'carList':['carData','system','colors','materials':{'color':'blue'}],'carData':{'colors':'yellow','name':'Chrysler'}}";
         p.deepJSON(carsJSON,"name");
+        //'John'
         p.deepJSON(carsJSON,"age");
+        //30
         p.deepJSON(carsJSON,"cars");
+        //{'car1':'Ford','carData':'BMW','car3':'Fiat','interior':{'material':'leather','color':'blue','keys':{'digital':'yes','physical':'no'}}}
         p.deepJSON(carsJSON,"carList.materials");
+        //{'color':'blue'}
         p.deepJSON(carsJSON,"cars.interior.keys.digital");
+        //'yes'
     }
 }
