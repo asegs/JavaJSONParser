fa147dd0894ad7721db6e965933782b9763bf51f
Aaron <54863852+asegs@users.noreply.github.com>
2021-04-01 11:58:39 -0400

    Update Examples.java

--- Examples.java
+++ Examples.java
@@ -2,15 +2,15 @@ public class Driver {
     public static void main(String[] args) {
         Parser p = new Parser();
         String carsJSON = "{'name':'John','age':30,'cars': {'car1':'Ford','carData':'BMW','car3':'Fiat','interior':{'material':'leather','color':'blue','keys':{'digital':'yes','physical':'no'}}},'carList':['carData','system','colors','materials':{'color':'blue'}],'carData':{'colors':'yellow','name':'Chrysler'}}";
-        p.deepJSON(carsJSON,"name");
-        //'John'
-        p.deepJSON(carsJSON,"age");
+        p.deepJSON(carsJSON,"name",true);
+        //'name':'John'
+        p.deepJSON(carsJSON,"age",false);
         //30
-        p.deepJSON(carsJSON,"cars");
-        //{'car1':'Ford','carData':'BMW','car3':'Fiat','interior':{'material':'leather','color':'blue','keys':{'digital':'yes','physical':'no'}}}
-        p.deepJSON(carsJSON,"carList.materials");
+        p.deepJSON(carsJSON,"cars",true);
+        //'cars':{'car1':'Ford','carData':'BMW','car3':'Fiat','interior':{'material':'leather','color':'blue','keys':{'digital':'yes','physical':'no'}}}
+        p.deepJSON(carsJSON,"carList.materials",false);
         //{'color':'blue'}
-        p.deepJSON(carsJSON,"cars.interior.keys.digital");
-        //'yes'
+        p.deepJSON(carsJSON,"cars.interior.keys.digital",true);
+        //'digital':'yes'
     }
 }
